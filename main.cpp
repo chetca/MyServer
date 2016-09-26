1eb32970e74009cb72be87310140ee42cb39ea72
chetca <chetca@yandex.ru>
2016-09-26 21:01:47 +0800

    Commentaries

--- main.cpp
+++ main.cpp
@@ -6,7 +6,9 @@ int main(int argc, char *argv[])
 {
     QCoreApplication a(argc, argv);
 
-    MyServer server;
+    MyServer server; //запускаем экземпляр сервера
+    //Согласен что быдлокод, не реализован даже банальный выход, но
+    // я замучался с серверной частью, и это уже результат. Потому и залил
 
     return a.exec();
 }
